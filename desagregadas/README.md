864e730f273ad3cf40dc17fe6cbdd027f6c6a7b2
Nicolas Torres <nickthor@dm3-1148la>
2016-12-05 23:00:40 -0500

    Script Borrador Desagregar Tablas

---
+++ desagregadas/README.md
@@ -0,0 +1,2 @@
+# SDIS_SIRBE
+Script borrador para desagregar tablas de la grilla de datos del proyecto SDIS_SIRBE
---
+++ desagregadas/rscript_sirbe_.R
@@ -0,0 +1,44 @@
+#Librerias 
+library(xlsx)
+library(openxlsx)
+library(extrafont)
+#Definir ubicación
+setwd("/home/nickthor/Projects/SDIS_SIRBE/analisis_102016/")
+#Cargar base de datos
+base_sdis <- read.csv("CargueSIRBEVejez.csv",header = T)
+#Construir una tabla con dos variables
+Frecuencia <- table(base_sdis$NOMTENVIV, base_sdis$EDAD_ACTUAL)
+#Hacer filtros de rangos
+Temprana <- Frecuencia[, 1:18]
+#Nombre de columnas
+colnames(Temprana)
+#Nombres filas
+rownames(Temprana)
+Tardia <- Frecuencia[, 19:57]
+colnames(Tardia)
+rownames(Tardia)
+Tardia
+Temprana
+#Sumar los valores numericos de la filas
+SumaTemprana<-rowSums(Temprana)
+SumaTardia<-rowSums(Tardia)
+#Sumar todos los valores numericos de la variable
+TotalTemprana <- sum(SumaTemprana)
+TotalTardia <- sum(SumaTardia)
+#Calcula el porcentaje
+PorcentajeTemprana <- (prop.table(SumaTemprana))
+PorcentajeTardia <- (prop.table(SumaTardia))
+PorcentajeTTotal <- (prop.table(SumaTardia))
+#Unir columnas y contruir una nueva tabla
+tablaTemprana <- cbind(SumaTemprana, PorcentajeTemprana)
+#Renombrasr columnas
+colnames(tablaTemprana ) <- c("Total", "%")
+tablaTardia <- cbind(SumaTardia, PorcentajeTardia)
+colnames(tablaTardia) <- c("Total", "%")
+#Guardar en una tabla nueva
+TempranaPrint <- as.table(tablaTemprana)
+TardiaPrint <- as.table(tablaTardia)
+#Crear lista de imprimibles
+lista_de_datos <- list("Vejez Temprana" = TempranaPrint, "Vejez Tardia" = TardiaPrint)
+#Imprime a un archivo de formato xlsx
+write.xlsx(lista_de_datos, file = "tablas/tabla.xlsx")
