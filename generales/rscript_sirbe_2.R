bca4c480e6290b0e010a4aa5a58e5f4db214fccb
JuanSebastianFlorez <jsflorezj@unal.edu.co>
2016-12-07 00:23:37 -0500

    primeras tablas de las generales

--- generales/rscript_sirbe_2.R
+++ generales/rscript_sirbe_2.R
@@ -3,11 +3,11 @@ library(xlsx)
 library(openxlsx)
 library(extrafont)
 #Definir ubicación
-setwd("/tmp/SDIS_SIRBE")
+setwd("/home/juanchoflo/Escritorio/UNAL2016-2/ConvenioDistrital/generales/")
 #Cargar base de datos
 base_sdis <- read.csv("CargueSIRBEVejez.csv",header = T)
 #Construir una tabla con dos variables
-Frecuencia <- table(base_sdis$NOMTENVIV, base_sdis$EDAD_ACTUAL)
+Frecuencia <- table(base_sdis$NOMNIVEDU, base_sdis$EDAD_ACTUAL)
 #Nombre de columnas
 colnames(Frecuencia)
 #Nombres filas
@@ -25,6 +25,6 @@ colnames(tablaFrecuencia) <- c("Total", "%")
 #Guardar en una tabla nueva
 FrecuenciaPrint <- as.table(tablaFrecuencia)
 #Crear lista de imprimibles
-lista_de_datos <- list("Generales" = FrecuenciaPrint)
+lista_de_datos <- list("NOMNIVEDU" = FrecuenciaPrint)
 #Imprime a un archivo de formato xlsx
-write.xlsx(lista_de_datos, file = "tablas/generales.xlsx")
+write.xlsx(lista_de_datos, file = "Tablas/NOMNIVEDU.xlsx")
